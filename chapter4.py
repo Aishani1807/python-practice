3e19960f5f45f136e8c847db82f044d9613bb841
Aishani Dutta <aparna.dutta3c@gmail.com>
2025-07-13 22:07:49 +0530

    Add files via upload

---
+++ chapter4.py
@@ -0,0 +1,18 @@
+'''fruits=[]
+for i in range(5):
+    f=input("Enter fruit name:")
+    fruits.append(f)
+print(fruits)
+
+marks=[]
+for i in range(6):
+    mark=int(input("Enter marks:"))
+    marks.append(mark)
+marks.sort()
+print(marks)
+
+a=(75,83,"Sonu")
+a[0]="Aishani
+
+l=[78,83,94,45,65,18,24]
+print(sum(l))'''
