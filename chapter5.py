898a66245cc4d705ed6bc765fb518dc6875017ec
Aishani Dutta <aparna.dutta3c@gmail.com>
2025-07-14 21:57:08 +0530

    Add files via upload

---
+++ chapter5.py
@@ -0,0 +1,25 @@
+'''words={"madad":"Help",
+"kursi":"chair",
+"billi":"cat"}
+word=input("Enter the word you want the meaning of:")
+print(words[word])
+
+s=set()
+for i in range(8):
+    n=int(input("Enter number:"))
+    s.add(n)
+print(s)
+
+q=set()
+n=int(input("Enter a no."))
+w=input("Enter a string")
+q.add(n)
+q.add(w)
+print(q)'''
+
+s={}
+for i in range(4):
+    name=input("Enter name:")
+    lang=input("Enter language")
+    s[name]=lang
+print(s)
