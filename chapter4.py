071f7c35a85e181a55490f076e6239e1cb37439a
Aishani Dutta <aparna.dutta3c@gmail.com>
2025-07-14 21:57:53 +0530

    Update chapter4.py

--- chapter4.py
+++ chapter4.py
@@ -1,4 +1,4 @@
-'''fruits=[]
+fruits=[]
 for i in range(5):
     f=input("Enter fruit name:")
     fruits.append(f)
@@ -15,4 +15,4 @@ a=(75,83,"Sonu")
 a[0]="Aishani
 
 l=[78,83,94,45,65,18,24]
-print(sum(l))'''
+print(sum(l))
